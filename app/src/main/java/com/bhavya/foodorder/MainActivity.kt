632407c919009e105e6233cb5cae5cb9f3f8fbc3
personal <sharmabhavya746@gamil.com>
2025-05-27 18:33:08 +0530

    init

---
+++ app/src/main/java/com/bhavya/foodorder/MainActivity.kt
@@ -0,0 +1,45 @@
+package com.bhavya.foodorder
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.bhavya.foodorder.navigation.FoNavigation
+import com.bhavya.foodorder.screens.GetStarted
+import com.bhavya.foodorder.ui.theme.FoodOrderTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContent {
+            FoodOrderTheme {
+
+              FoNavigation()
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    FoodOrderTheme {
+        Greeting("Android")
+    }
+}
