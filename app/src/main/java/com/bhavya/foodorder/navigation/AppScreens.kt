2366444ebfe97ee04a2698d8aefcb30dd3a1d314
personal <sharmabhavya746@gamil.com>
2025-05-31 00:54:04 +0530

    APPScreen

---
+++ app/src/main/java/com/bhavya/foodorder/navigation/AppScreens.kt
@@ -0,0 +1,8 @@
+package com.bhavya.foodorder.navigation
+
+enum class AppScreens(val route: String) {
+    GetStarted("get_started"),
+    LoginScreen("LoginScreen"),
+    AddFoodScreen("AddFoodScreen")
+
+}
